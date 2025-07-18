6066fef88080f5577b5d35ad7d93186de82a84aa
Ahbassetsh <89212154+Ahbassetsh@users.noreply.github.com>
2025-07-18 15:28:40 +0300

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,11 +4,12 @@ import torchvision
 
 from .coco import build as build_coco
 
+# 👇 ADD THIS for custom dataset support
+from .custom import build as build_custom
+
 
 def get_coco_api_from_dataset(dataset):
     for _ in range(10):
-        # if isinstance(dataset, torchvision.datasets.CocoDetection):
-        #     break
         if isinstance(dataset, torch.utils.data.Subset):
             dataset = dataset.dataset
     if isinstance(dataset, torchvision.datasets.CocoDetection):
@@ -19,7 +20,11 @@ def build_dataset(image_set, args):
     if args.dataset_file == 'coco':
         return build_coco(image_set, args)
     if args.dataset_file == 'coco_panoptic':
-        # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
         return build_coco_panoptic(image_set, args)
+    
+    # 👇 ADD THIS for your custom loader
+    if args.dataset_file == 'custom':
+        return build_custom(image_set, args)
+
     raise ValueError(f'dataset {args.dataset_file} not supported')
